a78c665ee6d240f8e8d956ef624ccb65319880cb
Mason <mason47895@gmail.com>
2024-02-21 19:06:45 -0500

    idk

--- problems/08-advanced-exponent.js
+++ problems/08-advanced-exponent.js
@@ -41,7 +41,13 @@ times `advancedExponent` is being recursively called.
 
 
 function advancedExponent(b, n) {
-  // Your code here 
+  if(n === 0) return 1;
+  if(n === 1) return b;
+  if(n % 2 === 0){
+    return advancedExponent(b, n/2) ** 2
+  }else{
+    return b * advancedExponent(b, (n - 1) / 2) ** 2
+  }
 }
 
 
